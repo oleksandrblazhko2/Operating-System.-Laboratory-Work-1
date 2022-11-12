aa13cc47f9d8f73521f7dbeffc93ae536ad22068
oleksandrblazhko2 <79433255+oleksandrblazhko2@users.noreply.github.com>
2022-11-12 08:03:53 +0200

    Update TestCases.java

--- TestCases.java
+++ TestCases.java
@@ -15,7 +15,7 @@ class TestCases {
             // create template string with PL/SQL-function "add_user"
 	    CallableStatement cstmt1 = con.prepareCall("{? = call add_user(?,?)}");
 	    cstmt1.registerOutParameter(1,Types.NUMERIC);		
-	    System.out.print("TC1: ");
+	    System.out.print("TC1: user 'user1' is not exists; add_user('user1','a12A345678#') = 1");
             Statement cstmt2 = con.createStatement();
 			cstmt2.executeUpdate("DELETE FROM Users WHERE uname = upper('user1')");
             // init template variables
@@ -29,12 +29,12 @@ class TestCases {
 	        System.out.println("Failed");
 	   	testCaseResult = -1;
 	    }
-    	    System.out.print("TC2: ");
+    	    System.out.print("TC2: user 'user1' is exists; add_user('user1','a12A345678#') = -1");
             // init template variables
 	    cstmt1.setString(2, "user1");
 	    cstmt1.setString(3, "a12A345678#");
 	    cstmt1.executeUpdate();
-            if (cstmt1.getInt(1) == -2) System.out.println("Passed");
+            if (cstmt1.getInt(1) == -1) System.out.println("Passed");
 	    else { 
 	        System.out.println("Failed");
 		testCaseResult = -1;
@@ -42,9 +42,7 @@ class TestCases {
    	    // close connect 
             con.close();  
         }
-	catch(Exception e) {
-		    System.out.println(e);
-	}  
+	catch(Exception e) { System.out.println(e); }  
 	System.exit(testCaseResult);
     }    
 }  
